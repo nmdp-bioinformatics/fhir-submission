bd272feb3c01009edf107fe97bf900fca02de873
Andrew S. Brown, Ph.D <abrown3@nmdp.org>
2017-08-15 16:02:51 -0500

    Updates

--- src/main/java/org/nmdp/fhirsubmission/util/FhirMessageUtil.java
+++ src/main/java/org/nmdp/fhirsubmission/util/FhirMessageUtil.java
@@ -25,23 +25,89 @@ package org.nmdp.fhirsubmission.util;
  */
 
 import org.nmdp.fhirsubmission.exceptions.FhirBundleSubmissionFailException;
+import org.nmdp.hmlfhirconvertermodels.attributes.FhirPrimaryResource;
+import org.nmdp.hmlfhirconvertermodels.attributes.FhirResource;
 import org.nmdp.hmlfhirconvertermodels.domain.fhir.FhirMessage;
+import org.nmdp.hmlfhirconvertermodels.domain.fhir.Patient;
 
-import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class FhirMessageUtil {
 
-   public static void submit(FhirMessage fhirMessage) throws FhirBundleSubmissionFailException {
-
-   }
-
-    private static List<Object> getPrimaryResources(FhirMessage message) {
-
+    public void submit(FhirMessage fhirMessage) throws Exception {
+        getPrimaryResources(fhirMessage);
     }
 
-    private static List<Object> traverseObject(Annotation annotation, Object obj) {
-       
+    private List<Object> getPrimaryResources(FhirMessage message) throws FhirBundleSubmissionFailException {
+        for (Patient patient : message.getPatients().getPatients()) {
+            //List<Field> declaredFields = Arrays.asList(patient.getClass().getDeclaredFields());
+            List<Field> declaredFields = getAllFields(patient.getClass());
+            List<Object> fhirPrimaryResources = getFieldsImplementingAnnotation(FhirPrimaryResource.class, declaredFields);
+            List<Object> fhirResources = getFieldsImplementingAnnotation(FhirResource.class, declaredFields);
+
+//            List<Object> fhirPrimaryResources = declaredFields.stream()
+//                    .filter(Objects::nonNull)
+//                    .map(field -> traverseObject(FhirPrimaryResource.class, field))
+//                    .collect(Collectors.toList());
+//
+//            List<Object> fhirResources = declaredFields.stream()
+//                    .filter(Objects::nonNull)
+//                    .map(field -> traverseObject(FhirResource.class, field))
+//                    .collect(Collectors.toList());
+        }
+
+        return Arrays.asList(new Object());
+    }
+
+    private List<Field> getAllFields(Class clazz) {
+        List<Field> fields = new ArrayList<>();
+
+        fields.addAll(Arrays.asList(clazz.getDeclaredFields()));
+
+        if (clazz.getSuperclass() != null) {
+            fields.addAll(getAllFields(clazz.getSuperclass()));
+        }
+
+        return fields;
+    }
+
+    private List<Object> traverseObject(Class<?> annotation, Object obj) {
+       List<Object> resources = new ArrayList<>();
+       List<Field> fields = Arrays.asList(obj.getClass().getDeclaredFields());
+
+       for (Field field : fields) {
+           try {
+              field.setAccessible(true);
+              resources.addAll(getFieldsImplementingAnnotation(annotation, field.get(obj)));
+              resources.addAll(traverseObject(annotation, field.get(obj)));
+           } catch (IllegalAccessException ex) {
+
+           }
+       }
+
+       return resources;
+    }
+
+    private List<Object> getFieldsImplementingAnnotation(Class<?> annotation, List<Field> fields) {
+        return fields.stream()
+                .filter(Objects::nonNull)
+                .filter(field -> Arrays.asList(field.getDeclaredAnnotations()).stream()
+                    .filter(Objects::nonNull)
+                    .anyMatch(ant -> ant.getClass().equals(annotation)))
+                .collect(Collectors.toList());
+    }
+
+    private List<Object> getFieldsImplementingAnnotation(Class<?> annotation, Object obj) {
+       return Arrays.asList(obj.getClass().getDeclaredFields()).stream()
+               .filter(Objects::nonNull)
+               .filter(field -> Arrays.asList(field.getDeclaredAnnotations()).stream()
+                       .filter(Objects::nonNull)
+                       .anyMatch(ant -> ant.getClass().equals(annotation)))
+               .collect(Collectors.toList());
     }
 }
